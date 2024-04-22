4eeeed6168ac54daf48cfb763183ccbe12ac0362
Giulia Cardieri <giulia.cardieri@gmail.com>
2024-04-22 16:55:37 -0400

    Update test email

--- src/api/contato/content-types/contato/lifecycles.js
+++ src/api/contato/content-types/contato/lifecycles.js
@@ -4,7 +4,7 @@ module.exports = {
 
     try {
       await strapi.plugins["email"].services.email.send({
-        to: "cesarcarone@cardieri.com.br",
+        to: "giulia.cardieri@gmail.com",
         from: "social@cardieri.com.br",
         cc: "giulia.cardieri@gmail.com",
         replyTo: result.Email,
