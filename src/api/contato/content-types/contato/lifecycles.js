d9dfa24b821e4e8503011d5948522ed97a47ea40
Giulia Cardieri <giulia.cardieri@gmail.com>
2024-04-22 17:04:24 -0400

    Remove cc

--- src/api/contato/content-types/contato/lifecycles.js
+++ src/api/contato/content-types/contato/lifecycles.js
@@ -6,7 +6,6 @@ module.exports = {
       await strapi.plugins["email"].services.email.send({
         to: "giulia.cardieri@gmail.com",
         from: "social@cardieri.com.br",
-        cc: "giulia.cardieri@gmail.com",
         replyTo: result.Email,
         subject: "Contato Site Cardieri - Não responda esse email diretamente!",
         html: `Nome: ${result.Nome}<br>Email: ${result.Email}<br>Assunto: ${result.Assunto}<br>Telefone: ${result.Telefone}<br>Mensagem: ${result.Mensagem}`,
